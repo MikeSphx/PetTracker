431d5cf85fc3acbd13d7c55ce93269112289f6e9
Dan Broun <dnlbroun@gmail.com>
2018-10-30 21:00:00 -0400

    Added manage docs
    table of appointments and modal for exports
    
    TODO: add modal for view

--- js/manage-docs.js
+++ js/manage-docs.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-    // Code that runs when page is loaded
+    registerEventHandlers();
 });
+
+function registerEventHandlers() {
+    $('#add-new-button').click(addNewButtonClick);
+    $('#export-button').click(exportButtonClick);
+}
+
+function addNewButtonClick() {
+    console.log('Clicked add new'); 
+}
+
+function exportButtonClick() {
+    console.log('Clicked export');
+    $('#exportModal').on('show.bs.modal', function (event) {
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+})
+}
+
